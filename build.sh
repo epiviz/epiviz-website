1682ab2d1a5bdb6d074121ebef46304f29343d4e
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-04-20 22:12:55 -0400

    update build to include polymer components

--- build.sh
+++ build.sh
@@ -1,11 +1,40 @@
+# remove everything in current dist
 rm -r dist/*
-# mkdir dist/
-hugo -s home/ -d ../dist/
-hugo -s tutorials/ -d ../dist/tutorials/
-mkdir dist/documentation
-cp -r documentation/ dist/
 
+# build epiviz hugo instance
+hugo -s home/ -d ../dist/
+
+# build tutorial hugo instance
+hugo -s tutorials/ -d ../dist/tutorials/
+
+# copy epiviz documentation
+cp -r documentation dist/documentation/app
+
+# build metaviz hugo instance
 hugo -s metaviz/ -d ../dist/metaviz/
+
+# copy metaviz documentation
 cp -r metavizDocumentation/dataprovider/ dist/metaviz/documentation/
 cp -r metavizDocumentation/metavizr/ dist/metaviz/documentation/
 cp metavizDocumentation/IntroToMetavizr.html dist/metaviz/documentation/IntroToMetavizr.html
+
+# build polymer documentation
+mkdir dist/polymer
+
+# build epiviz-chart documentation
+cp -r polymer-epiviz-chart dist/polymer/charts
+cd dist/polymer/charts
+bower install
+bower install hcbravolab/epiviz-chart
+sed -i 's/bower_components/components/g' components/epiviz-charts/index.html
+echo "<META http-equiv="refresh" content=\"0;URL=charts/components/epiviz-charts/\">" >index.html
+cd ../../..
+
+# build epiviz-data-source documentation
+cp -r polymer-epiviz-ds dist/polymer/datasource
+cd dist/polymer/datasource
+bower install
+bower install hcbravolab/epiviz-data-source
+sed -i 's/bower_components/components/g' components/epiviz-data-source/index.html
+echo "<META http-equiv="refresh" content=\"0;URL=datasource/components/epiviz-data-source/\">" >index.html
+cd ../../..
---
+++ home/content/documentation.md
@@ -0,0 +1,9 @@
++++
+date        = "2017-04-18T11:27:27-04:00"
+title       = "Epiviz Documentation"
++++
+
+* [Epiviz JS App documentation](http://epiviz.github.io/documentation/app/)
+* Epiviz Polymer Components
+    - [Data Source ](http://epiviz.github.io/polymer/datasource/)   
+    - [Charts](http://epiviz.github.io/polymer/charts/)
